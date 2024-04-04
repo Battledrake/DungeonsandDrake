d08adf536d774175a9e9fa990656dd1f53a30786
BattleDrake <battledrake@gmail.com>
2024-04-04 12:06:27 -0700

    Setup Basic Attributes
    Implemented Health/MaxHealth/Mana/ManaHealth attributes with replication.

--- Source/DungeonsandDrake/Private/AbilitySystem/DnDAttributeSet.cpp
+++ Source/DungeonsandDrake/Private/AbilitySystem/DnDAttributeSet.cpp
@@ -2,4 +2,42 @@
 
 
 #include "AbilitySystem/DnDAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UDnDAttributeSet::UDnDAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
+}
+
+void UDnDAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UDnDAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDnDAttributeSet, Health, OldHealth);
+}
+
+void UDnDAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDnDAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UDnDAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDnDAttributeSet, Mana, OldMana);
+}
+
+void UDnDAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDnDAttributeSet, MaxMana, OldMaxMana);
+}
