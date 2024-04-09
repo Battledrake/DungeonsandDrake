8629e8009496bbcfe209a8bbe41bf6f5e72a6623
BattleDrake <battledrake@gmail.com>
2024-04-08 19:27:34 -0700

    Pre and Post Attribute Changes
    Implemented beginning functionality to adjust attribute pre and post effect. Clamped health/mana.

--- Source/DungeonsandDrake/Private/AbilitySystem/DnDAttributeSet.cpp
+++ Source/DungeonsandDrake/Private/AbilitySystem/DnDAttributeSet.cpp
@@ -3,6 +3,9 @@
 
 #include "AbilitySystem/DnDAttributeSet.h"
 #include "Net/UnrealNetwork.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 UDnDAttributeSet::UDnDAttributeSet()
 {
@@ -20,6 +23,63 @@ void UDnDAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Out
 	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UDnDAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UDnDAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxMana());
+	}
+}
+
+void UDnDAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
+{
+
+	Props.EffectContextHandle = Data.EffectSpec.GetContext();
+	Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	if (IsValid(Props.SourceASC))
+	{
+		Props.SourceAvatarActor = Props.SourceASC->GetAvatarActor();
+		Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+		if (Props.SourceController == nullptr)
+		{
+			if (APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
+			{
+				Props.SourceController = Pawn->GetController();
+			}
+		}
+		if (Props.SourceController)
+		{
+			Props.SourceCharacter = Props.SourceController->GetCharacter();
+		}
+	}
+
+
+	if (AActor* TargetAvatarActor = Data.Target.GetAvatarActor())
+	{
+		Props.TargetAvatarActor = TargetAvatarActor;
+		Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		Props.TargetCharacter = Props.TargetController->GetCharacter();
+		Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
+	}
+}
+
+void UDnDAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FEffectProperties Props;
+	SetEffectProperties(Data, Props);
+
+
 }
 
 void UDnDAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
